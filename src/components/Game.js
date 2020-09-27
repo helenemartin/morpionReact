b9bf848d07d4069e07309c83dd9f8eecefd74175
Your Name <helenemartin13@hotmail.com>
2020-09-27 16:27:53 +0100

    add styling to squares

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import Board from  './Board';
 
-const Game = ( ) => <div>Morpion</div>
+const Game = ( ) => {
+
+    const handleClick = () => {
+
+    }
+    const jumpTo = () => {
+
+    }
+    const renderMoves = () => {
+
+    }
+
+    return (
+        <Board onClick={handleClick} />
+    )
+}
 
 export default Game;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+const style = {
+    background: 'beige',
+    border: '2px solid coral',
+    fontsize: '30px',
+    cursor: 'pointer',
+    outline: 'none',
+}
+
 const Square= ({value, onClick}) => (
-    <button onClick={onClick}>
+    <button style={style} onClick={onClick}>
         {value}
     </button>
 )
