7027bce3643a6d58958c05d7b8e29a8cbe76acfc
Your Name <helenemartin13@hotmail.com>
2020-09-27 16:34:38 +0100

    style the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Square from './Square';
 
+const style = {
+    border: '4px solid red',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
 const Board = ({ squares, onClick}) => (
-    <div>
+    <div style={style}>
         <Square  value="1" onClick={() => onClick("empty value")} />
         <Square  value="2" onClick={() => onClick("empty value")} />
         <Square  value="3" onClick={() => onClick("empty value")} />
