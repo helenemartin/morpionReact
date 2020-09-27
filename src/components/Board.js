45303ee838b3ba2280935a8cd4022f49a99bd465
Your Name <helenemartin13@hotmail.com>
2020-09-27 15:54:16 +0100

    destructure the square component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import Square from './Square';
 
-const Board = ( ) => <div>Board</div>
+const Board = ({ squares, onClick}) => (
+    <div>
+        <Square  value="1" onClick={() => onClick("empty value")} />
+        <Square  value="2" onClick={() => onClick("empty value")} />
+        <Square  value="3" onClick={() => onClick("empty value")} />
+        <Square  value="4" onClick={() => onClick("empty value")} />
+        <Square  value="5" onClick={() => onClick("empty value")} />
+        <Square  value="6" onClick={() => onClick("empty value")} />
+        <Square  value="7" onClick={() => onClick("empty value")} />
+        <Square  value="8" onClick={() => onClick("empty value")} />
+        <Square  value="9" onClick={() => onClick("empty value")} />
+    </div>
+)
 
 export default Board;
